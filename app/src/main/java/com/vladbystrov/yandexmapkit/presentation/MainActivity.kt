453421aa4ecc4bbc1627ea2ea56951b3d1096b5f
vladislavbystrov <keocera@icloud.com>
2022-10-16 13:19:10 +0300

    Добавил зависимости

--- app/src/main/java/com/vladbystrov/yandexmapkit/presentation/MainActivity.kt
+++ app/src/main/java/com/vladbystrov/yandexmapkit/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.vladbystrov.yandexmapkit
+package com.vladbystrov.yandexmapkit.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vladbystrov.yandexmapkit.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
