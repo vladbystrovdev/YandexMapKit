28d676af08ff848a6f32c5bb626bce0dcbb32ec6
vladislavbystrov <keocera@icloud.com>
2022-10-16 13:03:23 +0300

    Initial commit

---
+++ app/src/main/java/com/vladbystrov/yandexmapkit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vladbystrov.yandexmapkit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
